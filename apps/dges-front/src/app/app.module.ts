ab807f9524c7941572975719a46047d39b3f6a3d
GreedyA1 <m.vato122@gmail.com>
2020-12-05 13:22:03 +0400

    removed extra imports

--- apps/dges-front/src/app/app.module.ts
+++ apps/dges-front/src/app/app.module.ts
@@ -3,13 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UiModule } from '@dges/ui';
-import { ProjectsComponent } from './projects/projects.component';
-import { JobsComponent } from './jobs/jobs.component';
-import { CertificatesComponent } from './certificates/certificates.component';
-import { EducationsComponent } from './educations/educations.component';
-import { ResumeComponent } from './resume/resume.component';
-import { ContactComponent } from './contact/contact.component';
-import { TimelineComponent } from './timeline/timeline.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
