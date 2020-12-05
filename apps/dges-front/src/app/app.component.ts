fc61d52a41910c01f7342e77d9f08ff722cfad55
GreedyA1 <m.vato122@gmail.com>
2020-12-05 13:55:25 +0400

    add routerlink, dynamic routes in sidenav

--- apps/dges-front/src/app/app.component.ts
+++ apps/dges-front/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'dges-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'dges-front';
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  public fillerNav = []
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, router: Router) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.fillerNav.push(...router.config.filter(route => route.path).map(route => route.path))
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- apps/dges-front/src/app/app.module.ts
+++ apps/dges-front/src/app/app.module.ts
@@ -6,10 +6,23 @@ import { UiModule } from '@dges/ui';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatListModule} from '@angular/material/list';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, UiModule, AppRoutingModule, BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
