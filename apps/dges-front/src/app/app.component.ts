f419ce3e5c049fd3b02594adb500076e0991bd24
GreedyA1 <m.vato122@gmail.com>
2020-12-05 14:31:14 +0400

    prettier added, removed deprecated calls

--- apps/dges-front/src/app/app.component.ts
+++ apps/dges-front/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'dges-root',
   templateUrl: './app.component.html',
@@ -10,16 +10,22 @@ export class AppComponent implements OnDestroy {
   title = 'dges-front';
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
-  public fillerNav = []
+  public fillerNav = [];
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, router: Router) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    router: Router
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-    this.fillerNav.push(...router.config.filter(route => route.path).map(route => route.path))
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+    this.fillerNav.push(
+      ...router.config.filter((route) => route.path).map((route) => route.path)
+    );
   }
 
   ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
+    this.mobileQuery.removeEventListener('change',this._mobileQueryListener);
   }
 }
