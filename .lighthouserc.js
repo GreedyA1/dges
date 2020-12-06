a98a6d75c9de146f77590b30758ffe5a691ba221
GreedyA1 <m.vato122@gmail.com>
2020-12-06 19:03:59 +0400

    lighthouserc format fixed

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    ci: {
-      collect: {
-        numberOfRuns: 3,
-        staticDistDir: './dist/apps/dges-front',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      staticDistDir: './dist/apps/dges-front',
     },
-  };
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
