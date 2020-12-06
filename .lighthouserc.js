e01fa047468ae8ee32659812f785c18236ea2421
GreedyA1 <m.vato122@gmail.com>
2020-12-06 19:51:46 +0400

    rebuild hopefully

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      staticDistDir: './dist/apps/dges-front',
+      staticDistDir: './dist/dges-front',
     },
     upload: {
       target: 'temporary-public-storage',
