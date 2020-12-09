600be259bac9ec85c79b57b003b81b1d5eec0f0f
GreedyA1 <m.vato122@gmail.com>
2020-12-09 23:30:43 +0400

    changed circleCI config.yml to check deployed webpage

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,8 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      staticDistDir: './dist/apps/dges-front/',
+      url: ['https://dges.app/'],
+      // staticDistDir: './dist/apps/dges-front/',
     },
     upload: {
       target: 'temporary-public-storage',
