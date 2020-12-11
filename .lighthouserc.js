9d2731d3139190f6ab84dc2a24ec7a5e5bd6c36a
GreedyA1 <m.vato122@gmail.com>
2020-12-11 23:33:19 +0400

    secret added

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -8,5 +8,10 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    upload: {
+      target: 'lhci',
+      serverBaseUrl: 'https://limitless-lowlands-77781.herokuapp.com/',
+      token: '${{ secrets.LHCI_TOKEN }}', // could also use LHCI_TOKEN variable instead
+    },
   },
 };
