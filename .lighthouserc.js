426c4d046217203e5ca2f5f8bcd15e6d96b7b8fd
GreedyA1 <m.vato122@gmail.com>
2021-01-03 21:12:48 +0400

    test only lighthouse

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     upload: {
       target: 'lhci',
-      serverBaseUrl: '${LHCI_SERVER_BASE_URL}',
+      serverBaseUrl: '${LHCI_SERVER_BASE_URL} + asdas',
       token: '${LHCI_TOKEN}', // could also use LHCI_TOKEN variable instead
     },
   },
