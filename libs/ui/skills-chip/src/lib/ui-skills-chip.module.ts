9d7369e987181f1e1d89858e5f64a71324d15522
GreedyA1 <m.vato122@gmail.com>
2021-01-24 23:39:49 +0400

    chips changed to material

--- libs/ui/skills-chip/src/lib/ui-skills-chip.module.ts
+++ libs/ui/skills-chip/src/lib/ui-skills-chip.module.ts
@@ -2,10 +2,12 @@ import { Input, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillChipComponent } from './skill-chip/skill-chip.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 import { SkillsGroupChipsComponent } from './skills-group-chips/skills-group-chips.component';
 
 @NgModule({
-  imports: [CommonModule, MatTooltipModule],
+  imports: [CommonModule, MatTooltipModule, MatChipsModule, MatIconModule],
   declarations: [SkillChipComponent, SkillsGroupChipsComponent],
   exports: [SkillChipComponent, SkillsGroupChipsComponent],
 })
