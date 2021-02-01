52b435c62d37b83699feb6f6143e33fc2be2fbf3
GreedyA1 <m.vato122@gmail.com>
2021-02-01 21:31:37 +0400

    job-form added

--- libs/forms/job-form/src/lib/forms-job-form.module.ts
+++ libs/forms/job-form/src/lib/forms-job-form.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JobsFormComponent } from './jobs-form/jobs-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsChipsModule } from '@dges/forms/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    FormsChipsModule,
+    MatNativeDateModule,
+    MatProgressBarModule],
+  declarations: [JobsFormComponent],
+  exports: [JobsFormComponent],
 })
 export class FormsJobFormModule {}
---
+++ libs/forms/job-form/src/lib/jobs-form/jobs-form.component.ts
@@ -0,0 +1,153 @@
+import { Component, forwardRef, Input } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Skill } from '@dges/types/skill';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'dges-jobs-form',
+  templateUrl: './jobs-form.component.html',
+  styleUrls: ['./jobs-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => JobsFormComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => JobsFormComponent),
+      multi: true,
+    },
+  ],
+})
+export class JobsFormComponent implements ControlValueAccessor {
+  @Input() choiceArray: string[];
+
+  @Input()
+  get skills$(): Observable<Skill[]> {
+    return this._skills$;
+  }
+  set skills$(skills: Observable<Skill[]>) {
+    this._skills$ = skills;
+  }
+  _skills$: Observable<Skill[]>;
+
+  @Input()
+  get tools$(): Observable<Skill[]> {
+    return this._tools$;
+  }
+  set tools$(tools: Observable<Skill[]>) {
+    this._tools$ = tools;
+  }
+  _tools$: Observable<Skill[]>;
+
+  public isDisabled = false;
+  expression =
+    '/(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9]' +
+    '[a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})/gi';
+
+  public jobForm = new FormGroup({
+    id: new FormControl({ value: null, disabled: this.isDisabled }, []),
+    title: new FormControl({ value: '', disabled: this.isDisabled }, [
+      Validators.required,
+    ]),
+    description: new FormControl({ value: '', disabled: this.isDisabled }, [
+      Validators.required,
+    ]),
+    startDate: new FormControl({ value: '', disabled: this.isDisabled }, [
+      Validators.required,
+    ]),
+    endDate: new FormControl({ value: '', disabled: this.isDisabled }, [
+      Validators.required,
+    ]),
+    link: new FormControl({ value: '', disabled: this.isDisabled }, [
+      Validators.required,
+      // Validators.pattern(this.expression),
+    ]),
+    tools: new FormControl({ value: [], disabled: this.isDisabled }, [
+      Validators.minLength(0),
+    ]),
+    images: new FormControl({ value: [], disabled: this.isDisabled }, [
+      Validators.minLength(0),
+    ]),
+    skills: new FormControl({ value: [], disabled: this.isDisabled }, [
+      Validators.minLength(0),
+    ]),
+  });
+
+  // Function to call when the input is touched (when a star is clicked).
+  onTouched: () => void;
+
+  onBlur() {
+    this.onTouched();
+  }
+
+  writeValue(val: any): void {
+    val && this.jobForm.setValue(val);
+  }
+
+  registerOnChange(fn: any): void {
+    this.jobForm.valueChanges.subscribe(() => {
+      fn(this.jobForm.value);
+    });
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  validate(): ValidationErrors | null {
+    return this.jobForm.valid
+      ? null
+      : { subformerror: 'Problems in subform!' };
+  }
+
+  // uploadImages(imageURLS: string[]): void{
+  //   this.imagesFormControl.setValue(imageURLS);
+  // }
+
+  get titleFormControl(): AbstractControl {
+    return this.jobForm.controls['title'];
+  }
+
+  get descriptionFormControl(): AbstractControl {
+    return this.jobForm.controls['description'];
+  }
+
+  get startDateFormControl(): AbstractControl {
+    return this.jobForm.controls['startDate'];
+  }
+
+  get endDateFormControl(): AbstractControl {
+    return this.jobForm.controls['endDate'];
+  }
+
+  get linkFormControl(): AbstractControl {
+    return this.jobForm.controls['link'];
+  }
+
+  get toolsFormControl(): AbstractControl {
+    return this.jobForm.controls['tools'];
+  }
+
+  get imagesFormControl(): AbstractControl {
+    return this.jobForm.controls['images'];
+  }
+
+  get skillsFormControl(): AbstractControl {
+    return this.jobForm.controls['skills'];
+  }
+}
