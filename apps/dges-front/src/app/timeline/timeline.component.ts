428e1443c20327e5e7f38a31cad287cd90ee2a8f
GreedyA1 <m.vato122@gmail.com>
2021-02-05 23:21:06 +0400

    parallax started

--- apps/dges-front/src/app/timeline/timeline.component.ts
+++ apps/dges-front/src/app/timeline/timeline.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent implements OnInit {
+
+  yo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+
   constructor() {
     console.log('');
   }
 
   ngOnInit(): void {
     console.log('');
+  }
+
+  random(){
+    return Math.random(); 
   }
 }
