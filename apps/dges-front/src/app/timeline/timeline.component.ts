4dc332b707db10180618ac811ba05a9ab27809e6
GreedyA1 <m.vato122@gmail.com>
2021-02-11 22:55:43 +0400

    timeline fixed scroll

--- apps/dges-front/src/app/timeline/timeline.component.ts
+++ apps/dges-front/src/app/timeline/timeline.component.ts
@@ -5,8 +5,8 @@ import { ProjectsFacade } from '@dges/store/projects/firebase';
 import { Education } from '@dges/types/education';
 import { Job } from '@dges/types/job';
 import { Project } from '@dges/types/project';
-import { combineLatest, Observable, zip } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'dges-timeline',
@@ -22,6 +22,9 @@ export class TimelineComponent implements OnInit {
 
   allCards$;
 
+  timelineLastYear: number;
+  timelineFirstYear: number;
+
   constructor(
     @Inject('JobsFacade') private jobsFacade: JobsFacade,
     @Inject('EducationFacade') private educationFacade: EducationFacade,
@@ -36,30 +39,51 @@ export class TimelineComponent implements OnInit {
     this.jobs$ = this.jobsFacade.allJobs$;
     this.educations$ = this.educationFacade.allEducation$;
     this.projects$ = this.projectsFacade.allProjects$;
-    this.allCards$ = combineLatest([this.jobs$, this.educations$, this.projects$])
-      .pipe(
-        map(([jobs$, educations$, projects$]) => [
-          ...jobs$.map((job: Job) => {
-            const jobWithType: Job & { type: string } = { ...job, type: 'job' };
-            return jobWithType;
-          }),
-          ...educations$.map((education: Education) => {
-            const educationWithType: Education & { type: string } = {
-              ...education,
-              type: 'education',
-            };
-            return educationWithType;
-          }),
-          ...projects$.map((project: Project) => {
-            const projectWithType: Project & { type: string } = {
-              ...project,
-              type: 'project',
-            };
-            return projectWithType;
-          }),
-        ]),
-        map((a) => a.sort((a, b) => a.endDate.getTime() - b.endDate.getTime()))
-      )
+    this.allCards$ = combineLatest([
+      this.jobs$,
+      this.educations$,
+      this.projects$,
+    ]).pipe(
+      map(([jobs$, educations$, projects$]) => [
+        ...jobs$.map((job: Job) => {
+          const jobWithType: Job & { type: string } = { ...job, type: 'job' };
+          return jobWithType;
+        }),
+        ...educations$.map((education: Education) => {
+          const educationWithType: Education & { type: string } = {
+            ...education,
+            type: 'education',
+          };
+          return educationWithType;
+        }),
+        ...projects$.map((project: Project) => {
+          const projectWithType: Project & { type: string } = {
+            ...project,
+            type: 'project',
+          };
+          return projectWithType;
+        }),
+      ]),
+      map((a) =>
+        a.sort((a, b) => {
+          const year = a.endDate.getFullYear();
+          if (year <= this.timelineLastYear) {
+            this.timelineLastYear = year;
+          }
+          if (year > this.timelineFirstYear) {
+            this.timelineFirstYear = year;
+          }
+          return a.endDate.getTime() - b.endDate.getTime();
+        })
+      ),
+      tap(() => {
+        for (let i = this.timelineFirstYear; i < this.timelineFirstYear; i++) {
+          this.years = [];
+          this.years.push(i);
+        }
+        this.years = [].concat(this.years)
+      })
+    );
     this.jobsFacade.init();
     this.projectsFacade.loadProjects();
     this.educationFacade.loadEducation();
@@ -73,6 +97,8 @@ export class TimelineComponent implements OnInit {
   }
 
   setStyle(number: number) {
-    return "url('https://source.unsplash.com/random/" + number + "/?wallpapers')";
+    return (
+      "url('https://source.unsplash.com/random/" + number + "/?wallpapers')"
+    );
   }
 }
